095efd447baf72e2261293716cf6385ce1c51437
Cleiton Junior Mittmann <mittmannv8@gmail.com>
2021-01-05 15:41:48 -0300

    Added skeleton to manage the experiments

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch,
+  useLocation
+} from "react-router-dom";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { RiReactjsLine } from "react-icons/ri";
 
-function App() {
+export const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      <li>
+        <Link to="/exp1">Experiment One</Link>
+      </li>
+    </ul>
+  );
+};
+
+export const Exp1 = () => {
+  return <div>Exp 1</div>;
+};
+
+const Header = () => {
+  const location = useLocation();
+
+  const isHome = location.pathname === "/";
+  const appName = isHome ? "Home" : location.pathname;
+
+  return (
+    <div className="Header">
+      {isHome && <RiReactjsLine className="Icon home-icon" />}
+      {!isHome && (
+        <Link className="GoBackButton" to="/">
+          <IoMdArrowRoundBack className="Icon" />
+        </Link>
+      )}
+
+      <h1 className="AppName">{appName}</h1>
     </div>
   );
-}
+};
 
-export default App;
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/exp1" component={Exp1} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
